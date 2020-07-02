5a4d80a52d9a05d0dd13e9a2ba30151d2871c185
scott-vincent <scottvincent7@gmail.com>
2020-07-02 08:35:53 +0100

    add remote module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/scott-vincent/go-hello
 
 go 1.14
+
+require github.com/google/go-cmp v0.5.0
--- hello.go
+++ hello.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/scott-vincent/go-hello/morestrings"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseString("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
